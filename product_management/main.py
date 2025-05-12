8cefa679efa9d7167e848599bfc954da8b31088d
Suman Gole <gole35830@gmail.com>
2025-05-12 14:28:28 +0545

    refactor: pass topics and events to be handled to dispatcher from service

--- product_management/main.py
+++ product_management/main.py
@@ -5,12 +5,23 @@ from flask import request
 from product_management.application.services.product_service import ProductService
 from product_management.domain.aggregates.product import Product
 from product_management.infrastructure.product_repository import ProductRepository
+from shared.abstractions.events.event import Event
+from shared.infrastructure.messaging.kafka_producer import get_dispatcher
 from shared.utils.create_service import create_service
+
+KAFKA_PRODUCT_MANAGEMENT_TOPICS_MAP = {
+    "ProductCreated": "product-topic",
+}
+
+
+dispatcher = get_dispatcher(kafka_topic_map=KAFKA_PRODUCT_MANAGEMENT_TOPICS_MAP)
 
 
 def main():
     product_body = request.get_json()
-    service, uow = create_service(ProductRepository, ProductService)
+    service, uow = create_service(
+        ProductRepository, ProductService, dispatcher=dispatcher
+    )
 
     with uow:
         product = Product.create(id=uuid.uuid4(), **product_body)
